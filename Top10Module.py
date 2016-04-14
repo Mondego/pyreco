8b99c66adee1a82055f58e6ce4412f46a2dc6a02
andrearenika <andrearenika@gmail.com>
2016-04-14 00:07:33 -0700

    Script to determine the top 10 modules

--- Top10Module.py
+++ Top10Module.py
@@ -1,120 +1,82 @@
-from GraphNode import GraphNode
-from operator import itemgetter
 import json
+import sys
+import multiprocessing as mp
+from ASTUtils import DFGraph, AssignmentNode
+from collections import Counter
 import os
 
 """
 Script to get the most frequently used modules
 """
 
-frequency = {}
-queries = {}
-libs_used = []
+def find_libs(df_graph, q):
+    lib_count=Counter()
+    for node in df_graph.dfs():
+        if isinstance(node,AssignmentNode):
+            for src in node.src:
+                pos=src.find('.')
+                if pos!=-1:
+                    try:
+                        lib=src[:pos]
+                        if lib!='self':
+                            lib_count[lib]+=1
+                    except ImportError:
+                        pass
+    q.put(lib_count)
+    return
 
-def convert_to_dict(node):
-    node_dict={}
-    node_dict['src']=node.src
-    node_dict['op']=node.op
-    node_dict['tgt']=node.tgt
-    node_dict['lNo']=node.lineNum
-    node_dict['colOff']=node.colOffset
-    return node_dict
-
-def add_to_queries(lib, node, folder, file):
-    if lib in queries.keys():
-        if folder in queries[lib].keys():
-            if file in queries[lib][folder].keys():
-                node_obj=convert_to_dict(node)
-                if node_obj not in queries[lib][folder][file]:
-                    queries[lib][folder][file].append(
-                        convert_to_dict(node))
-            else:
-                queries[lib][folder][file]=[
-                    convert_to_dict(node)]
+def listener(q):
+    lib_counter=Counter()
+    while(1):
+        counter=q.get()
+        if isinstance(counter, Counter):
+            lib_counter+=counter
         else:
-            queries[lib][folder]={file:[
-                convert_to_dict(node)]}
-    else:
-        queries[lib]={folder:
-            {file:[
-            convert_to_dict(node)]}}
+            break
+    with open("Top10.txt","w") as f:
+        for lib, freq in lib_counter.most_common(20):
+            f.write(lib+':'+str(freq)+'\n')
 
-def find_libs(graph, folder, file):
-    lib_callees={}
-    for node in graph:
-        if '.' in node.src and node.op == '--becomes--':
-            pos = node.src.find('.')
-            lib = node.src[:pos] if pos!=-1 else ''
-            if len(lib)!=0 and ':' not in lib:
-                try:
-                   # __import__(lib)
-                    if lib not in libs_used:
-                        libs_used.append(lib)
-                    lib_callees[node.tgt]=lib
-                    add_to_queries(lib, node, folder, file)
-                except ImportError:
-                    pass
-                except:
-                    pass
-        elif node.op == '--calls--':
-            if node.src in lib_callees.keys():
-                lib=lib_callees[node.src]
-                add_to_queries(lib, node, folder, file)
-        else:
-            if node.src in lib_callees.keys():
-                del lib_callees[node.src]
+def main():
+    manager=mp.Manager()
+    q=manager.Queue()
+    pool=mp.Pool(mp.cpu_count())
 
-with open('graph_new.txt') as f:
-    current_graph = []
-    folderName = ''
-    fileName = ''
-    for line in f:
-        if line == '':
-            pass
-        if line.startswith('Foldername'):
-            folderName = line.split(':')[1]
-            print folderName
-            pass
-        elif line.startswith('-' * 20):
-            if current_graph != []:
-                find_libs(current_graph,
-                          folderName,
-                          fileName)
-        elif line.startswith('__FILENAME__'):
-            if current_graph != []:
-                find_libs(current_graph,
-                          folderName,
-                          fileName)
-                current_graph = []
-            fileName = line.split()[2]
-            print '\tfile:'+fileName
-        elif len(line.split()) == 2:
-            node = line.split()
-            current_graph.append(
-                GraphNode(node[0], node[1], ''))
-        elif len(line.split()) == 3:
-            node = line.split()
-            current_graph.append(
-                GraphNode(node[0], node[1], node[2]))
-        elif len(line.split()) == 5:
-            node = line.split()
-            current_graph.append(
-                GraphNode(node[0], node[1], node[2],
-                          node[3], node[4]))
+    watcher = pool.apply_async(listener,(q,))
 
-for lib in libs_used:
-    frequency[lib]=0
-    for file in queries[lib].keys():
-        frequency[lib] += len(file)
+    jobs=[]
 
-f1 = open('Top10.txt', 'w')
-count = 0
-for lib, freq in sorted(frequency.items(), key=itemgetter(1), reverse=True):
-    count += 1
-    f1.write(lib + '\t' + str(freq) + '\n')
-    f2=open('queries1/Queries_'+lib+'.txt', 'w')
-    data=queries[lib]
-    json.dump(data, f2, sort_keys=True, indent = 4, ensure_ascii=False)
-    if count==10:
-        break
-f.close()
+    graph_folder='graphs/'
+    for f_name in os.listdir(graph_folder):
+        graph=""
+        with open(graph_folder+'/'+f_name,'r') as file:
+            for line in file:
+                if line.strip()=='-' * 20:
+                    try:
+                        df_graphs=json.loads(graph)
+                        for g in df_graphs['files']:
+                            print 'Folder:'+df_graphs['folder'],"File:"+g['file']
+                            df_graph=DFGraph.deserialize(g['graph'])
+                            job=pool.apply_async(find_libs,(df_graph,q))
+                            jobs.append(job)
+                        graph=""
+                    except:
+                        print "Unexpected error in worker:", sys.exc_info()[0]
+                        with open("check-json.txt",'w') as f:
+                            f.write(graph)
+                        break
+                else:
+                    graph+=line
+
+    for job in jobs:
+        try:
+            job.get()
+        except:
+            continue
+
+    q.put('kill')
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    main()
