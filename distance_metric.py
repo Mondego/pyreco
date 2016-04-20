0bdd3235356c8702c277b77e660deae239a83570
dyangUCI <Di@vcv066072.vpn.uci.edu>
2016-04-19 20:43:27 -0700

    jaccard similarity

--- distance_metric.py
+++ distance_metric.py
@@ -3,7 +3,12 @@
 # intersection/union
 
 def jaccard_similarity(x, y):
+	
 	set_x = set(x)
 	set_y = set(y)
-	return s.intersection(y) / s.union(y)
+
+	# empty sets are always different
+	if len(set_x)==0 or len(set_y)==0:
+		return 0.0
+	return len(set_x & set_y) / len(set_x | set_y)
 	
