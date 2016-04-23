81dc9d88d68a994ec1e94980cfeed5a5e4c7d2f3
andrearenika <andrearenika@gmail.com>
2016-04-22 18:58:58 -0700

    Removed mp from Jedi eval

--- jedi_test.py
+++ jedi_test.py
@@ -7,6 +7,11 @@ import json
 
 LIB="os"
 FOLDS=10
+f=list()
+sum_prec=list()
+count=list()
+
+
 
 def compute_r_precision(jedi_results, relevant_results):
     relevant_set=set(relevant_results)
@@ -19,7 +24,7 @@ def compute_r_precision(jedi_results, relevant_results):
     return p/float(r_length)
 
 
-def run_queries_for_prj(fold_no, query_text, q):
+def run_queries_for_prj(fold_no, query_text):
     prj_queries=json.loads(query_text)
     def run_query(query_info):
         with open('repoData/'+folder+'/allPythonContent.py', 'r') as f:
@@ -38,7 +43,7 @@ def run_queries_for_prj(fold_no, query_text, q):
                     for completion in script.completions():
                         results.append(completion.complete)
                 p=compute_r_precision(results, query_info["results"])
-                q.put((fold_no-1, results, query_info["results"], p))
+                write_results((fold_no-1, results, query_info["results"], p))
 
     for file_queries in prj_queries["q_list"]:
         folder=prj_queries["folder"]
@@ -47,74 +52,55 @@ def run_queries_for_prj(fold_no, query_text, q):
         for query in file_queries["queries"]:
             run_query(query)
 
-def listener(q):
-    f=list()
-    sum_prec=list()
-    count=list()
+def write_results(msg):
+    if isinstance(msg,tuple):
+        n,compl_results,relevant_results,p=msg
+        f[n].write("Completion results:"+str(compl_results)+"\n")
+        f[n].write("Relevant results:"+str(relevant_results)+"\n")
+        f[n].write("R-Precision:"+str(p*100)+"\n")
+        f[n].write('-' * 20 + '\n')
+        f[n].flush()
+        sum_prec[n]+=p
+        count[n]+=1
 
-    for i in range(FOLDS):
-        f.append(open('results-jedi/results-'+str(i+1)+'.txt','w'))
-        sum_prec.append(0)
-        count.append(0)
-
-    while(1):
-        msg=q.get()
-        if isinstance(msg,tuple):
-            n, compl_results, relevant_results, p=msg
-            f[n].write("Completion results:"+str(compl_results)+"\n")
-            f[n].write("Relevant results:"+str(relevant_results)+"\n")
-            f[n].write("R-Precision:"+str(p*100)+"\n")
-            f[n].write('-' * 20 + '\n')
-            f[n].flush()
-            sum_prec[n]+=p
-            count[n]+=1
-
-        else:
-            f_summary=open('results-jedi/results-summary.txt','w')
-            for i in range(FOLDS):
-                f[i].close()
+    else:
+        f_summary=open('results-jedi/results-summary.txt','w')
+        for i in range(FOLDS):
+            f[i].close()
+            if count[i]!=0:
                 f_summary.write("Fold:"+str(i+1)+"\n")
                 f_summary.write("Num_queries:"+str(count[i])+"\n")
                 f_summary.write("Avg Precision:"+str((sum_prec[i]/float(count[i]))*100)+"\n")
                 f_summary.write('-' * 20 + '\n')
-            f_summary.close()
-            break
+        f_summary.close()
 
 
 def main():
-    manager=mp.Manager()
-    pool=mp.Pool(mp.cpu_count())
-
-    q=manager.Queue()
-    l=manager.Lock()
-    watcher=pool.apply_async(listener,(q,))
-
-    jobs=[]
-    count=0
+    counter=0
     query_file='queries/queries-'+LIB+'.txt'
+
+    for i in range(FOLDS):
+            f.append(open('results-jedi/results-'+str(i+1)+'.txt','w'))
+            sum_prec.append(0)
+            count.append(0)
+
     with open(query_file,'r') as file:
         query=""
         for line in file:
             if line.strip()=='-' * 20:
                 try:
-                    job=pool.apply_async(run_queries_for_prj,(count%FOLDS+1,query, q))
-                    jobs.append(job)
+                    run_queries_for_prj(counter%FOLDS+1,query)
                     query=""
-                    count+=1
+                    counter+=1
+                    if counter>2:
+                        break
                 except:
                     print "Unexpected error in worker:", sys.exc_info()[0]
             else:
                 query+=line
 
-    for job in jobs:
-        try:
-            job.get()
-        except:
-            continue
+    write_results('summary')
 
-    q.put('kill')
-    pool.close()
-    pool.join()
 
 if __name__ == '__main__':
     main()
